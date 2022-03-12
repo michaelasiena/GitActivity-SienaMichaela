98a3c28b73782d857f980dcaa073cdc2687905bc
SienaMichaela <michaelasiena74@gmail.com>
2022-03-12 21:14:36 +0800

    My Educational Background added

--- git-python-siena.py
+++ git-python-siena.py
@@ -1,3 +1,6 @@
 print ("Michaela Alperes Siena")
 print ("Laylay, Boac, Marinduque")
 print ("February 4, 2002")
+print ("College of Engineering, Information and Industrial Technology")
+print ("Bachelor of Science in Information Technology")
+print ("2D")
